eb7ad01ef3692c0931fd361ec1cf12a563e875f3
Bryan Atencia <33098684+bryan0767@users.noreply.github.com>
2018-05-30 18:47:44 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ var expect      = require('chai').expect;
 var cors        = require('cors');
 
 var apiRoutes         = require('./routes/api.js');
-var fccTestingRoutes  = require('./routes/fcctesting.js');
-var runner            = require('./test-runner');
 
 var app = express();
 
@@ -17,12 +15,9 @@ app.use(helmet())
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.use(cors({origin: '*'})); //For FCC testing purposes only
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Sample front-end
 app.route('/b/:board/')
   .get(function (req, res) {
     res.sendFile(process.cwd() + '/views/board.html');
@@ -33,29 +28,19 @@ app.route('/b/:board/:threadid')
     res.sendFile(process.cwd() + '/views/thread.html');
   });
 
-//Index page (static HTML)
 app.route('/')
   .get(function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
   });
 
-//For FCC testing purposes
-fccTestingRoutes(app);
-
-//Routing for API 
 apiRoutes(app);
 
-//Sample Front-end
-
-    
-//404 Not Found Middleware
 app.use(function(req, res, next) {
   res.status(404)
     .type('text')
     .send('Not Found');
 });
 
-//Start our server and tests!
 app.listen(process.env.PORT || 3000, function () {
   console.log("Listening on port " + process.env.PORT);
   if(process.env.NODE_ENV==='test') {
